68330d3ee748ad094fd2f846e9f3d339718ac5be
Vedant Mistry <44vedantmistry@gmail.com>
2023-07-02 08:53:17 +0530

    Update mainf.java

--- mainf.java
+++ mainf.java
@@ -68,7 +68,7 @@ class myoptions implements fiction, nonfiction, bsnbooks, science_fantasy, novel
         System.out.println("News of the Day 1");
         System.out.println("News of the Day 2");
         System.out.println("News of the Day 3");
-        System.out.println("News 4");
+        System.out.println("News of the Day 4");
         System.out.println("News 5");
         System.out.println("News 6");
     }
